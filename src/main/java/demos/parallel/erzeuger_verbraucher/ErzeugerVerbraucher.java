a9fd140a5b937c4e40bd35023b4f549ec288e986
Julian Osmer <julian.osmer@outlook.de>
2019-11-18 15:06:36 +0100

    update

--- src/main/java/demos/parallel/erzeuger_verbraucher/ErzeugerVerbraucher.java
+++ src/main/java/demos/parallel/erzeuger_verbraucher/ErzeugerVerbraucher.java
@@ -4,10 +4,10 @@ import java.util.Vector;
 
 public class ErzeugerVerbraucher {
     public static void main(String[] args) {
-        Vector v = new Vector();
-        Producer p = new Producer(v);
-        Consumer c = new Consumer(v);
-        p.start();
-        c.start();
+        Vector vector = new Vector();
+        Producer producer = new Producer(vector);
+        Consumer consumer = new Consumer(vector);
+        producer.start();
+        consumer.start();
     }
 }
